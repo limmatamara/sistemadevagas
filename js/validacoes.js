007b4cc61da4b71a41f2fead4897821ec00fc54a
Victor Trajano <trajanodev@hotmail.com>
2021-11-06 20:32:12 -0400

    Finalização de validações de cadastro

--- js/validacoes.js
+++ js/validacoes.js
@@ -1,11 +1,28 @@
 
 //#region Cadastro Usuário
+const validarTipoDeUsuario= () => {
+    let select = document.getElementById('tipoUsuario')
+    let tipoDeUsuario = select.options[select.selectedIndex].value
+
+    let primeiroEmprego = document.getElementById('primeiroEmprego')
+    let labelPrimeiroEmprego = document.getElementById('labelPrimeiroEmprego')
+
+    if(tipoDeUsuario === 'recrutador'){
+        primeiroEmprego.setAttribute('class' , 'esconder')
+        labelPrimeiroEmprego.setAttribute('class' , 'esconder')
+    }else{
+        primeiroEmprego.setAttribute('class' , '')
+        labelPrimeiroEmprego.setAttribute('class' , '')
+    }
+    console.log(tipoDeUsuario)
+}
+
 function validarNome() {
     ehValido = false
     let nome = document.getElementById('nomeCompleto').value
     let arrayName = [...nome]
     let erroNome = document.getElementById('nome-completo-error')
-    let regex = /[a-zA-Z\s]+/
+    let regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/
 
     let isNameValid = arrayName.every(nome => regex.test(nome) === true)
 
@@ -38,8 +55,6 @@ const validarData = () => {
         ehValido ? erroData.setAttribute('class', 'esconder') : erroData.setAttribute('class', 'error');
 
     return ehValido;
-
-
 }
 
 const adicionarMascaraData = (input, data) => {
@@ -62,6 +77,43 @@ const adicionarMascaraData = (input, data) => {
         }
     }
 }
+
+const validaEmail = () => {
+    let regex = /\S+@\S+\.\S+/;
+    let emailInput = document.getElementById('emailCadastro');
+    let ehValido = false;
+
+    let emailValido = regex.test(emailInput.value);
+
+    if(emailValido){
+        ehValido = true;
+    }
+
+    console.log(emailValido)
+    
+    let emailError = document.getElementById('email-registration-error')
+    emailInput.value === '' ? emailError.setAttribute('class', 'esconder') :
+    ehValido ? emailError.setAttribute('class' , 'esconder') : emailError.setAttribute('class', 'error')
+}
+
+const validaSenha = () => {
+    let ehValido = false;
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    let senhaInput = document.getElementById('senha');
+    let senhaValida = regex.test(senhaInput.value);
+
+    if(senhaValida){
+        ehValido = true
+    }
+
+    let erroSenha = document.getElementById('senha-registration-erro')
+    senhaInput.value === '' ? erroSenha.setAttribute('class' , 'esconder') :
+    ehValido ? erroSenha.setAttribute('class' , 'esconder') : erroSenha.setAttribute('class' , 'error')
+
+}
+
+
+
 const validaCadastroUsuario = () => {
 
 }
